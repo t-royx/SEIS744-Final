188f83d52276e082acf2d68031d08eaf6f7c3d98
t-royx <Eric5709@Stthomas.edu>
2017-12-15 18:38:51 -0600

    Update Temperature Zone Control Smart App.groovy

--- Temperature Zone Control Smart App.groovy
+++ Temperature Zone Control Smart App.groovy
@@ -1,7 +1,6 @@
 /**
  *  Temperature Zone Controller
- *
- *  Copyright 2017 
+ * 
  *  Author: Troy Erickson
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
